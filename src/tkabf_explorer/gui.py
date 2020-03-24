a9a80fb12bdef67d595479fae1324b03a8915f93
Nicholas George <nicholas.george32@gmail.com>
2020-03-24 11:28:48 -0600

    basic layout and plot setup

--- src/tkabf_explorer/gui.py
+++ src/tkabf_explorer/gui.py
@@ -1,44 +1,59 @@
-import tkinter
-from matplotlib.backends.backend_tkagg import (
-    FigureCanvasTkAgg, NavigationToolbar2Tk)
-# Implement the default Matplotlib key bindings.
-from matplotlib.backend_bases import key_press_handler
+import sys
+import tkinter as tk
+from tkinter import ttk
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import NavigationToolbar2Tk
 from matplotlib.figure import Figure
-import numpy as np
+# Implement the default Matplotlib key bindings.
+#from matplotlib.backend_bases import key_press_handler
 
-root = tkinter.Tk()
-root.wm_title("Embedding in Tk")
+#import numpy as np
+LARGE_FONT = ("Verdana", 12)
 
-fig = Figure(figsize=(5, 4), dpi=100)
-t = np.arange(0, 3, .01)
-fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
-
-canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
-canvas.draw()
-canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
-
-toolbar = NavigationToolbar2Tk(canvas, root)
-toolbar.update()
-canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
-
-
-def on_key_press(event):
-    print("you pressed {}".format(event.key))
-    key_press_handler(event, canvas, toolbar)
-
-
-canvas.mpl_connect("key_press_event", on_key_press)
+class TkAbfExplorer(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        tk.Tk.wm_title(self,"tkABF Viewer v0.1-dev")
+        container = tk.Frame(self, bg="blue")
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(1, weight=3)
+        container.example = "container example"
+        control_frame = ControlFrame(container, self)
+        plot_frame = PlotFrame(container, self)
+        control_frame.grid(row=0,column=0, sticky="nsew")
+        plot_frame.grid(row=0,column=1, sticky="nsew")
 
 
-def _quit():
-    root.quit()     # stops mainloop
-    root.destroy()  # this is necessary on Windows to prevent
-                    # Fatal Python Error: PyEval_RestoreThread: NULL tstate
+class ControlFrame(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent, bg='red')
+        label = tk.Label(self, text="controls", font = LARGE_FONT)
+        label.pack(pady=10,padx=10)
+        # in our case, container is "parent" and can be accessed like so:
+        print(parent.example)
 
-button = tkinter.Button(master=root, text="Quit", command=_quit)
-button.pack(side=tkinter.BOTTOM)
 
-tkinter.mainloop()
-# If you put root.destroy() here, it will cause an error if the window is
-# closed with the window manager.
-print("done")
+class PlotFrame(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Main plot", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+        f = Figure(figsize=(5,5), dpi=100)
+        a = f.add_subplot(111)
+        a.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+        #toolbar = NavigationToolbar2Tk(canvas, self)
+        #toolbar.update()
+        canvas._tkcanvas.pack()
+
+
+app = TkAbfExplorer()
+app.mainloop()
+print("Done")

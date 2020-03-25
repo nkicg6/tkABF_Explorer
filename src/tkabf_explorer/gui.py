85c22f79e754f629bc64335abc8e5df42fbe5196
Nicholas George <nicholas.george32@gmail.com>
2020-03-25 11:07:59 -0600

    listbox containing list of abf's and the functions to manipulate it

--- src/tkabf_explorer/gui.py
+++ src/tkabf_explorer/gui.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import tkinter as tk
 from tkinter import ttk
@@ -40,7 +41,7 @@ class TkAbfExplorer(tk.Tk):
 
 class ControlFrame(tk.Frame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent, bg='grey')
+        tk.Frame.__init__(self, parent, bg='white')
         # frame button/info layout
         self.grid_rowconfigure(0, weight=1) # choose dir button
         self.grid_rowconfigure(1, weight=2) # dir display
@@ -49,20 +50,49 @@ class ControlFrame(tk.Frame):
         self.grid_columnconfigure(1,weight=2)
         self.label = ttk.Label(self, text="test option", font = LARGE_FONT)
         self.main_dir_button = ttk.Button(self, text="Choose a directory",
-                                         command=self.open_dir)
+                                          command=self.open_dir)
+        # setup scrollbox for files
+        self.list_of_files = tk.Listbox(self)
+        self.list_of_files.insert(0,"No abf files found")
+        self.scrollbar = tk.Scrollbar(self, orient="vertical")
+        self.scrollbar.config(command=self.list_of_files.yview)
+        self.list_of_files.config(yscrollcommand=self.scrollbar.set)
         self.entry_text = ttk.Entry(self)
+        # layout
+        self.scrollbar.grid(row=1,column=1,sticky="ns")
+        self.list_of_files.grid(row=1,column=0, sticky="ns",padx=5)
         self.label.grid(row=2, column=2)
         self.main_dir_button.grid(row=0)
         self.entry_text.grid(row=2)
+
     def on_button(self, event):
         gotvar = self.entry_text.get()
         self.label.config(text=gotvar)
 
-    def open_dir(self):
-        print("open dir activated")
-        self.working_dir = tk.filedialog.askdirectory()
-        print(self.working_dir)
+    def add_files_to_listbox(self):
+        self.list_of_files.delete(0,tk.END)
+        keys_sorted = sorted([k for k in self.abf_list_dict.keys()])
+        for abf_file in keys_sorted:
+            self.list_of_files.insert(tk.END, abf_file)
 
+    def set_listbox_empty(self):
+        self.list_of_files.delete(0,tk.END)
+        self.list_of_files.insert(0, "No abf files found")
+        self.abf_list_dict = {}
+
+    def list_abfs(self):
+        abf_list = [i for i in os.listdir(self.working_dir) if i.endswith(".abf")]
+        self.abf_list_dict = {}
+        for item in abf_list:
+            self.abf_list_dict[item] = os.path.join(self.working_dir,item)
+        if len(abf_list) ==0:
+            self.set_listbox_empty()
+        else:
+            self.add_files_to_listbox()
+
+    def open_dir(self):
+        self.working_dir = tk.filedialog.askdirectory()
+        self.list_abfs()
 
 
 class PlotFrame(tk.Frame):

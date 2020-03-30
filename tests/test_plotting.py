fdad10996d622feec3459de5630a21b3c0487ffd
Nicholas George <nicholas.george32@gmail.com>
2020-03-30 12:29:02 -0600

    added tests for basic type checking

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -4,8 +4,17 @@ import pytest
 from tkabf_explorer import plotting
 
 
-#{"x":[], "y1":[[]], "x_label":"x", "y1_label":"y", "y2":[[]], "y2_label":"y",
-#"fontsize":16, "sweep_label":[], "linewidth":4, "y2_color":"black"}
+{"x":[], # type: numpy array or list; length must be == to the length of each y1 and y2 sub-array
+ "y1":[[]], # type: list of numpy arrays; number of sub arrays must equal number of sub arrays for y2. Length of x, y1 (each subarray), y2 (each subarray) must be equal
+ "x_label":"x", # type: string
+ "y1_label":"y", # type: string
+ "y2":[[]], # type: list of numpy arrays; see constraints on y1
+ "y2_label":"y", # type: string
+"fontsize":16, # type: int
+ "sweep_label":[], # type: list of strings; length of list of strings must be == to length of arrays in y1 and y2
+ "linewidth":4, # type: int
+ "y2_color":"black" # type: string
+}
 
 
 def test_check_len_equals():
@@ -14,21 +23,67 @@ def test_check_len_equals():
     y_short = [1]
     y_empty = []
     y_with_list = [[1,3,2]]
-    assert 1==2
+    plotting._check_len_equals(x,y) #should pass
+    plotting._check_len_equals(y_empty,y_empty)
+    with pytest.raises(AssertionError):
+        plotting._check_len_equals(x,y_short)
+    with pytest.raises(AssertionError):
+        plotting._check_len_equals(x,y_with_list)
+    with pytest.raises(AssertionError):
+        plotting._check_len_equals(y_short,y_with_list)
 
 def test_check_is_not_num_or_string():
     x = 1
     x1 = []
-    x2 = [[1,2,3], []]
-    x3 = [[1,2,3], 1]
-    x4 = [[1,2,3], "string"]
-    assert 1==2
+    x2 = "string"
+    x3 = [[1,2,3], [1]]
+    x4 = None
+    plotting._check_is_not_num_or_string(x1)
+    plotting._check_is_not_num_or_string(x3)
+    with pytest.raises(AssertionError):
+        plotting._check_is_not_num_or_string(x)
+    with pytest.raises(AssertionError):
+        plotting._check_is_not_num_or_string(x2)
+    with pytest.raises(AssertionError):
+        plotting._check_is_not_num_or_string(x4)
+
 
 def test_must_be_num_or_string():
-    assert 1==2
+    test_1 = {"x_label":[],
+              "y_label":[],
+              "y2_label":[],
+              "fontsize":[],
+              "linewidth":[]}
+    test_pass = {"x_label":"string",
+                 "y_label":"string",
+                 "y2_label":"string",
+                 "fontsize":1,
+                 "linewidth":3}
+    missing_key = {"x_label":"string",
+                   "y2_label":"string",
+                   "fontsize":1,
+                   "linewidth":3}
+    plotting._must_be_num_or_string(test_pass)
+    with pytest.raises(KeyError):
+        plotting._must_be_num_or_string(missing_key)
+    with pytest.raises(AssertionError):
+        plotting._must_be_num_or_string(test_1)
+
 
 def test_subarrays_must_be_equal():
-    assert 1==2
+    test1 = {"y1":[],"y2":[]}
+    test2 = {"y1":[[1,2,3]],"y2":[[1,2,3]]}
+    test3 = {"y1":[[1,2]],"y2":[[1,2,3]]}
+    test4 = {"y1":[[]],"y2":[[1,2,3]]}
+    test5 = {"yy1":[[]],"y2":[[1,2,3]]}
+    plotting._subarrays_must_be_equal(test1)
+    plotting._subarrays_must_be_equal(test2)
+    with pytest.raises(AssertionError):
+        plotting._subarrays_must_be_equal(test3)
+    with pytest.raises(AssertionError):
+        plotting._subarrays_must_be_equal(test4)
+    with pytest.raises(KeyError):
+        plotting._subarrays_must_be_equal(test5)
 
 def test_check_dim_x_dim_each_y_arr():
     assert 1==2
--- tkabf_explorer/plotting.py
+++ tkabf_explorer/plotting.py
@@ -2,7 +2,12 @@
 
 def _check_len_equals(x,y):
     """raises assertion error if len(x) != len(y)"""
-    assert len(x) == len(y)
+    try:
+        assert type(x[0]) == type(y[0]) # controls for comparing a list of lists to a list of len 1
+    except IndexError as e:
+        print(f"empty lists? Exception is: {e}")
+    finally:
+        assert len(x) == len(y)
 
 def _check_is_not_num_or_string(x):
     """raises assertion error is type is not expected. Type should be numpy array or list.
@@ -12,15 +17,23 @@ def _check_is_not_num_or_string(x):
 def _must_be_num_or_string(m):
     """These keys must be an int, float or string, cannot be a container (list, tuple, numpy array, etc.)"""
     must_be_num_or_string = ["x_label", "y_label", "y2_label", "fontsize", "linewidth"]
-    for i in must_be_num_or_string:
-        assert isinstance(m[i], (str, float, int))
+    try:
+        for i in must_be_num_or_string:
+            assert isinstance(m[i], (str, float, int))
+    except KeyError as e:
+        print("`_must_be_num_or_string` missing key! Error is: {e}")
+        raise(KeyError("Missing key in `_must_be_num_or_string`"))
 
 def _subarrays_must_be_equal(m):
     """each sub-array in y1 and y2 must have the same length as the others"""
     must_equal = ["y1", "y2"]#, "sweep_label", ]
-    for i in must_equal:
-        for ind,arr in enumerate(m['y1']):
-            _check_len_equals(m['y1'][ind], m[i][ind])
+    try:
+        for i in must_equal:
+            for ind,arr in enumerate(m['y1']):
+                _check_len_equals(m['y1'][ind], m[i][ind])
+    except KeyError as e:
+        print("`_subarrays_must_be_equal` missing key! Error is: {e}")
+        raise(KeyError("Missing key in `_subarrays_must_be_equal"))
 
 def _check_dim_x_dim_each_y_arr(m):
     """length of the x array must equal the length of EACH sub array in y1 and y2"""

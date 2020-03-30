297e16af4c49e978135448aefbf182ddb2db2d66
Nicholas George <nicholas.george32@gmail.com>
2020-03-29 21:22:27 -0600

    start working on tests and plotting module

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,2 +1,53 @@
 # test plotting.py functions
-from .context import
+from . import context
+import pytest
+from tkabf_explorer import plotting
+
+
+#{"x":[], "y1":[[]], "x_label":"x", "y1_label":"y", "y2":[[]], "y2_label":"y",
+#"fontsize":16, "sweep_label":[], "linewidth":4, "y2_color":"black"}
+
+
+def test_check_len_equals():
+    x = [1,2,3]
+    y = [1,2,3]
+    y_short = [1]
+    y_empty = []
+    y_with_list = [[1,3,2]]
+    plotting._check_len_equals(x,y)
+    with pytest.raises(AssertionError):
+        plotting._check_len_equals(x,y_short)
+    with pytest.raises(AssertionError):
+        plotting._check_len_equals(x,y_empty)
+    with pytest.raises(AssertionError):
+        plotting._check_len_equals(x,y_with_list)
+
+
+
+def test_check_is_not_num_or_string():
+    x = 1
+    x1 = []
+    x2 = [[1,2,3], []]
+    plotting._check_is_not_num_or_string(x1)
+    plotting._check_is_not_num_or_string(x2)
+    with pytest.raises(AssertionError):
+        plotting._check_is_not_num_or_string(x)
+
+def test_validate_data():
+    x = [1,2,3]
+    y1 = [4,5,6]
+    y1_1 = [5,6,7]
+    y2 = [1.5,2.5,3.5]
+    y2_1 = [i*0.1 for i in y2]
+    labels = ["one" , "two"]
+    test_map1 = {'x':x, "y1":[y1,y1_1],"y2":[y2,y2_1], "sweep_label":labels}
+    test_map2 = {'x':x, "y1":[],"y2":[y2,y2_1], "sweep_label":labels}
+    test_map3 = {'x':x, "y1":[y1],"y2":[y2,y2_1]}
+    test_map4 = {'x':x, "y1":["on"],"y2":[y2,y2_1], "sweep_label":labels}
+    assert test_map1 == plotting.validate_data(test_map1)
+    with pytest.raises(AssertionError):
+        plotting.validate_data(test_map2)
+    with pytest.raises(AssertionError):
+        plotting.validate_data(test_map3)
+    with pytest.raises(AssertionError):
+        plotting.validate_data(test_map4)
--- tkabf_explorer/plotting.py
+++ tkabf_explorer/plotting.py
@@ -1,7 +1,21 @@
 # plotting helper functions
 
+def _check_len_equals(x,y):
+    """raises assertion error if len(x) != len(y)"""
+    assert len(x) == len(y)
 
-def validate_date(m):
+def _check_is_not_num_or_string(x):
+    """raises assertion error is type is not list"""
+    assert not isinstance(x, (str, float, int, type(None)))
+    for item in x:
+        print(item)
+        assert not isinstance(item, (str,float,int, type(None)))
+
+
+def validate_data(m):
     """verify that the map passed in has appropriate dimensions"""
     m = m.copy()
+    must_equal = ["y1", "y2", "sweep_label", ]
+    must_be_len_1 = ["x", "x_label", "y_label", "y2_label", "fontsize", "linewidth"]
+    assert len(m["y1"]) == len(m["y2"]), f"problem with input data {m} in function `validate_data`"
     return m
